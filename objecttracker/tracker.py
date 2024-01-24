d7754622ab96ab4854fe19b29be7d68579c0635c
edblu1 <edgarblumenthal@gmx.de>
2024-01-24 11:51:04 +0100

    refactoring

--- objecttracker/tracker.py
+++ objecttracker/tracker.py
@@ -12,103 +12,100 @@ from visionlib.pipeline.tools import get_raw_frame_data
 
 from .config import ObjectTrackerConfig
 
-
 logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s %(message)s')
 logger = logging.getLogger(__name__)
 
-GET_DURATION = Histogram('object_tracker_get_duration', 'The time it takes to deserialize the proto until returning the detection result as a serialized proto',
+GET_DURATION = Histogram('object_tracker_get_duration',
+                         'The time it takes to deserialize the proto until returning the detection result as a serialized proto',
                          buckets=(0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25))
 MODEL_DURATION = Summary('object_tracker_tracker_update_duration', 'How long the tracker update takes')
 OBJECT_COUNTER = Counter('object_tracker_object_counter', 'How many objects have been tracked')
-PROTO_SERIALIZATION_DURATION = Summary('object_tracker_proto_serialization_duration', 'The time it takes to create a serialized output proto')
-PROTO_DESERIALIZATION_DURATION = Summary('object_tracker_proto_deserialization_duration', 'The time it takes to deserialize an input proto')
+PROTO_SERIALIZATION_DURATION = Summary('object_tracker_proto_serialization_duration',
+                                       'The time it takes to create a serialized output proto')
+PROTO_DESERIALIZATION_DURATION = Summary('object_tracker_proto_deserialization_duration',
+                                         'The time it takes to deserialize an input proto')
 
 
 class Tracker:
-    def __init__(self, config: ObjectTrackerConfig, cur_tracker="deepocsort") -> None:
+    def __init__(self, config: ObjectTrackerConfig, cur_tracker) -> None:
         self.config = config
         logger.setLevel(self.config.log_level.value)
 
         self.object_id_seed = uuid.uuid4()
         self.cur_tracker = cur_tracker
         self._setup()
-        
+
     def __call__(self, input_proto, *args, **kwargs) -> Any:
         return self.get(input_proto)
 
     @GET_DURATION.time()
     @torch.no_grad()
-    def get(self, input_proto):        
+    def get(self, input_proto):
         input_image, detection_proto = self._unpack_proto(input_proto)
-    
+
         inference_start = time.monotonic_ns()
         det_array = self._prepare_detection_input(detection_proto)
 
         tracking_output_array = self.tracker.update(det_array, input_image)
-        #print(tracking_output_array)
+        # print(tracking_output_array)
 
         OBJECT_COUNTER.inc(len(tracking_output_array))
-        
+
         inference_time_us = (time.monotonic_ns() - inference_start) // 1000
 
         num_cars = len(det_array)
 
         return self._create_output(tracking_output_array, detection_proto, inference_time_us, num_cars)
-        
+
     def _setup(self):
-        
-            
-        if self.cur_tracker == "boxmot_ocsort":
+
+        if self.cur_tracker == "ocsort_b":
             logger.info('Setting up object-tracker Boxmot: OCSort')
 
-            from boxmot.trackers.ocsort.ocsort import OCSort
+            from .trackingimpl.boxmot.trackers.ocsort.ocsort import OCSort
             self.tracker = OCSort()
-            
 
-        elif self.cur_tracker == "boxmot_botsort":
+        elif self.cur_tracker == "botsort_b":
             logger.info('Setting up object-tracker Boxmot: BotSort')
 
-            from boxmot.trackers.botsort.bot_sort import BoTSORT
+            from .trackingimpl.boxmot.trackers.botsort.bot_sort import BoTSORT
             self.tracker = BoTSORT(
-                Path(self.config.tracking_params.model_weights), 
-                torch.device(self.config.device), 
+                Path(self.config.tracking_params.model_weights),
+                torch.device(self.config.device),
                 self.config.tracking_params.fp16
-                )
-       
-        
-        elif self.cur_tracker == "boxmot_bytetrack":
+            )
+
+        elif self.cur_tracker == "bytetrack_b":
             logger.info('Setting up object-tracker Boxmot: Bytetrack')
 
-            from boxmot.trackers.bytetrack.byte_tracker import BYTETracker
+            from .trackingimpl.boxmot.trackers.bytetrack.byte_tracker import BYTETracker
             self.tracker = BYTETracker(
                 frame_rate=5
-                )
-            
-            
-        elif self.cur_tracker == "boxmot_deepocsort":
+            )
+
+        elif self.cur_tracker == "deepocsort_b":
             logger.info('Setting up object-tracker Boxmot: DeepOCSort')
 
-            from boxmot.trackers.deepocsort.deep_ocsort import DeepOCSort
+            from .trackingimpl.boxmot.trackers.deepocsort.deep_ocsort import DeepOCSort
             self.tracker = DeepOCSort(
-                Path(self.config.tracking_params.model_weights), 
-                torch.device(self.config.device), 
+                Path(self.config.tracking_params.model_weights),
+                torch.device(self.config.device),
                 self.config.tracking_params.fp16
-                )
-        
-        else:
+            )
+
+        elif self.cur_tracker == "deepocsort_og":
             logger.info('Setting up object-tracker DeepOCSort')
 
             from .trackingimpl.deepocsort.ocsort import OCSort
             self.tracker = OCSort(
-                Path(self.config.tracking_params.model_weights), 
-                torch.device(self.config.device), 
-                self.config.tracking_params.fp16, 
+                Path(self.config.tracking_params.model_weights),
+                torch.device(self.config.device),
+                self.config.tracking_params.fp16,
                 self.config.tracking_params.det_thresh,
             )
-        
-        
 
     PROTO_DESERIALIZATION_DURATION.time()
+
     def _unpack_proto(self, detection_proto_raw):
         detection_proto = DetectionOutput()
         detection_proto.ParseFromString(detection_proto_raw)
@@ -117,7 +114,7 @@ class Tracker:
         input_image = get_raw_frame_data(input_frame)
 
         return input_image, detection_proto
-    
+
     def _prepare_detection_input(self, detection_proto: DetectionOutput):
         det_array = np.zeros((len(detection_proto.detections), 6))
         for idx, detection in enumerate(detection_proto.detections):
@@ -129,8 +126,9 @@ class Tracker:
             det_array[idx, 4] = detection.confidence
             det_array[idx, 5] = detection.class_id
         return det_array
-    
+
     PROTO_SERIALIZATION_DURATION.time()
+
     def _create_output(self, tracking_output, detection_proto: DetectionOutput, inference_time_us, num_cars):
         output_proto = TrackingOutput()
         output_proto.frame.CopyFrom(detection_proto.frame)
@@ -138,7 +136,7 @@ class Tracker:
         # The length of detections and tracking_output can be different 
         # (as the latter only includes objects that could be matched to an id)
         # Therefore, we can only reuse the VideoFrame and have to recreate everything else
-        
+
         for pred in tracking_output:
             tracked_detection = output_proto.tracked_detections.add()
             tracked_detection.detection.bounding_box.min_x = int(pred[0])
@@ -154,8 +152,8 @@ class Tracker:
                 tracked_detection.detection.confidence = float(pred[5])
             else:
                 tracked_detection.detection.confidence = float(pred[6])
-  
+
         output_proto.metrics.CopyFrom(detection_proto.metrics)
         output_proto.metrics.tracking_inference_time_us = inference_time_us
-        
+
         return output_proto.SerializeToString(), num_cars, inference_time_us, tracking_output
