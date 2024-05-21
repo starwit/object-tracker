c4c54854cfea16148cf959ae70789f0bbb98cc1c
flonix8 <flstanek@googlemail.com>
2024-05-21 16:57:29 +0200

    Version 3.0.4: Fix excessive threading overhead by limiting Numpy to one thread (uses all CPU threads by default)

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Prevent numpy from using multiple threads, because we do not need to parallelize the computation and it created a lot of overhead
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 from objecttracker import run_stage
 
 if __name__ == '__main__':
